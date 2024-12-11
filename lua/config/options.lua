162821fa191263bb3039d5f60f08e30deaceb4b3
jg <jihonggan1@gmail.com>
2024-12-10 22:22:45 -0800

    tab settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,2 +1,8 @@
 vim.opt.number = true         -- Show absolute line numbers
 vim.opt.relativenumber = true -- Show relative line numbers
+
+-- Tab settings
+vim.opt.tabstop = 2      -- Number of spaces tabs count for
+vim.opt.shiftwidth = 2   -- Size of an indent
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.softtabstop = 2  -- Number of spaces that a <Tab> counts for
