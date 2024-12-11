5a8056e0916adc03af632e1d4545236815834b02
Jay Gan <jihonggan@google.com>
2024-12-10 16:59:11 -0800

    show line numbers

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("config.lazy")
 require("config.colortheme")
 require("config.evil_lualine")
+require("config.options")
---
+++ lua/config/options.lua
@@ -0,0 +1,2 @@
+vim.opt.number = true         -- Show absolute line numbers
+vim.opt.relativenumber = true -- Show relative line numbers
